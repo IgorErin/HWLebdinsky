cb5ee0bc87d6515170bb8533148d0053b1ff48d9
Igor Erin <47027759+IgorErin@users.noreply.github.com>
2021-11-27 12:57:33 +0300

    Create 39-3.c

---
+++ 39-3.c
@@ -0,0 +1,41 @@
+define _CRT_SECURE_NO_WARNINGS //interpolycia
+
+#include<stdio.h>
+#include<math.h>
+
+
+int interpolationSearch(int array[], int size, int n)
+{
+    int low = 0;
+    int mid;
+    int high = size - 1;
+
+    while (n > array[low] && n < array[high])
+    {
+        if (array[low] == array[high])
+            break;
+
+        mid = low + (n - array[low]) * (high - low) / (array[low] - array[high]);
+        if (n < array[mid])
+            high = mid - 1;
+        else if (n > array[mid])
+            low = mid + 1;
+        else
+            return mid;
+
+        if (array[low] == n)
+            return low;
+        else if (array[high] == n)
+            return high;
+    }
+    return -1;
+}
+
+
+
+
+int main() {
+    int x[15] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+    
+}
+
