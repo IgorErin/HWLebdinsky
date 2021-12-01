3e9a01abe3cc747dad0164836ff297ea1f1efb9d
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-01 10:44:21 +0300

    Update 39-8.c
    add a norm solve

--- 39-8.c
+++ 39-8.c
@@ -1,30 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS //interpolycia
+#include<stdio.h>
 #include<math.h>
-#include <time.h>
+#include<time.h>
 
-int fun(int **x, int *y, int left, int right, int m, int ind) { // binsearch
-	if (ind == m - 1)
+#define SIZE1 3
+#define SIZE2 3
+
+int relation(int* x, int* y, int n)
+{
+	int flag = 0;
+	for (int i = 0; i < n; i++)
 	{
-		return left;
-	}
-	int left1, right1, a = 0;
-
-	for (int i = left; i < right; i++) {
-		if (x[i][ind] == y[ind])
+		if (x[i] > y[i])
 		{
-			left1 = i;
-			a = 1;
+			flag = 1;
+			break;
 		}
-		if (a == 1 && x[i][ind] != y[ind]) {
-			right1 = i;
-			return fun(x, y, left1, right1, m, ind + 1);
+		if (x[i] < y[i])
+		{
+			flag = -1;
+			break;
 		}
 	}
 
-	if (a == 0) {
-		return -1;
-	}
+	return flag;
 }
 
-int main() {
 
- }
+
+int fun(int (x)[SIZE1][SIZE2], int* y, int low, int high) 
+{ 
+	
+
+	int mid = (high + low) / 2;
+
+	if (relation(x[mid], y, SIZE2) == 0)
+	{
+		return mid;
+	}
+	
+	if (low == high)
+	{
+		return -1;
+	}
+
+	if (relation(x[mid], y, SIZE2) > 0)
+	{
+		fun(x, y, low, mid);
+	}
+
+	if (relation(x[mid], y, SIZE2) < 0)
+	{
+		fun(x, y, mid + 1, high);
+	}
+
+}
+
+
+int main() {
+	int x[SIZE1][SIZE2];
+	int y[SIZE2];
+}
