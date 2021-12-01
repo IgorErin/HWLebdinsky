18a6218640d644c759eafd66362d9b97a3469ce6
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-01 10:08:30 +0300

    Update 39-3.c
    interpol fixed

--- 39-3.c
+++ 39-3.c
@@ -1,41 +1,38 @@
-define _CRT_SECURE_NO_WARNINGS //interpolycia
-
+#define _CRT_SECURE_NO_WARNINGS //interpolycia
 #include<stdio.h>
 #include<math.h>
-
 
 int interpolationSearch(int array[], int size, int n)
 {
     int low = 0;
     int mid;
     int high = size - 1;
-
-    while (n > array[low] && n < array[high])
+    while (1)
     {
-        if (array[low] == array[high])
-            break;
-
-        mid = low + (n - array[low]) * (high - low) / (array[low] - array[high]);// границы проверка
-        if (n < array[mid])
-            high = mid - 1;
-        else if (n > array[mid])// не работает.
-            low = mid + 1;
-        else
-            return mid;
-
+        
         if (array[low] == n)
             return low;
         else if (array[high] == n)
             return high;
+
+        if (array[low] == array[high])
+            break;
+        mid = low + (n - array[low]) * (high - low) / (array[high] - array[low]);
+        
+        if (n < array[mid])
+            high = mid;
+        
+        else if (n > array[mid])
+            low = mid + 1;
+        else
+            return mid;
+        
+        
     }
     return -1;
 }
-
-
-
-
 int main() {
     int x[15] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
-    
+    printf("%d", interpolationSearch(x, 15, 12));
 }
 
