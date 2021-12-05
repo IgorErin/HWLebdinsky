dea54107df2aab636a7b31546f8d78d8b5e8e971
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-05 09:57:32 +0300

    Update 43-3.c
    add norm solv

--- 43-3.c
+++ 43-3.c
@@ -2,18 +2,19 @@
 #include<stdio.h>
 #include<math.h>
 #include<time.h>
-#include<locale.h>
+#include<stdlib.h>
+
+
 
 #define SIZ 500
-
 
 // C:\\Users\\Igor\\Desktop\\t.txt
 int main()
 {
-	char* s = (char*)malloc(SIZ * sizeof(char) + 1);
-	char* s1 = (char*)malloc(SIZ * sizeof(char) + 1);
+    char* s = (char*)malloc(SIZ * sizeof(char) + 1);
+    char* s1 = (char*)malloc(SIZ * sizeof(char) + 1);
 
-	scanf("%s", s);
+    scanf("%s", s);
     scanf("%s", s1);
 
     FILE* in, * out;
@@ -21,12 +22,13 @@ int main()
 
     in = fopen(s, "r");
     out = fopen(s1, "w+");
-    
+    int i;
 
     while (!feof(in))
     {
-        ch = getc(in);
-        putc(ch, out);
+        fread(&ch, sizeof(char), 1, in);
+        fwrite(&ch, sizeof(char), 1, out);
+        //printf("%c", ch);
     }
 
     fclose(in);
