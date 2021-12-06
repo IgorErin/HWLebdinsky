c6535f527377be397be8ef46a521c39d8bf9e610
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-06 12:44:47 +0300

    Create 44-4b.c

---
+++ 44-4b.c
@@ -0,0 +1,15 @@
+#define POINTERTONEL(TYPE) \
+struct Node_##TYPE* PointerToN_##TYPE(struct Node_##TYPE *f, TYPE n) \
+{ \
+    struct Node_##TYPE* p = f; \
+    int i = 0; \
+    while (p != 0) \
+    { \
+        i++; \
+        p = p->n; \
+        if (i == n) \
+        { \
+            return p; \
+        } \
+    } \
+}
