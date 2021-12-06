e989c7c1ce2295f233c4fff9a69490e73493b9ea
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-06 12:44:14 +0300

    Create 44-4.c

---
+++ 44-4.c
@@ -0,0 +1,14 @@
+struct node* pointerToNEl(struct node* f, int n)
+{
+    struct node* p = f;
+    int i = 0;
+    while (p != 0)
+    {
+        i++;
+        p = p->n;
+        if (i == n)
+        {
+            return p;
+        }
+    }
+}
