4f3dd30a58a45b86b9e58d46cd8535331c7cf5e8
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-06 12:47:29 +0300

    Create 44-7a.c

---
+++ 44-7a.c
@@ -0,0 +1,13 @@
+int firstWhitData(struct node* f, int dat)
+{
+    struct node* p = f;
+    while (p != 0)
+    {
+        if (p->d == dat)
+        {
+            return p;
+        }
+        p = p->n;
+    }
+    return 0;
+}
