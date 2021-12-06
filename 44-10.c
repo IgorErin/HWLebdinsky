8492e4fdccab392c6eb7bda2c0c6af61ad42e1a2
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-06 23:22:17 +0300

    Update 44-10.c

--- 44-10.c
+++ 44-10.c
@@ -1,43 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include<stdio.h>
-#include<math.h>
-#include <time.h>
-
-#define SIZE 500
-
-struct node 
-{
-    int d;
-    struct node* n;
-};
-
-#define NODE(TYPE) \
-struct Node_##TYPE \
-{ \
-TYPE d; \
-struct Node_##TYPE *n; \
-};
-
-
-void printNode(struct node* f)
-{
-    struct node* p = f;
-    while (p != 0)
-    {
-        printf("%d \n", p->d);
-        p = p->n;
-    }
-}
-void add(struct node** f, int i)
-{
-    struct node* p = (struct node*)malloc(sizeof(struct node));
-
-    p->n = *f;
-    p->d = i;
-    *f = p;
-}
-
-
 struct node* addInWithPointer(struct node* f, int n, int dat)
 {
     struct node* p = f;
@@ -79,42 +39,23 @@ void addInWithPointerToPointer(struct node** f, int n, int dat)
     {
         if (i == n)
         {
-            p_new->n = (*pp);
-            (*pp) = p_new;
+            p_new->n = *pp;
+            *pp = p_new;
             
         }
        
         
         i++;
+        pp = &((*p).n);
         p = p->n;
-        pp = &p;
+        
         
     }
-    printf("\n");
-}
-
-int main()
-{
-    struct node first;
-    first.d = 0;
-    first.n = 0;
-
-    struct node* p_first = &first;
-    struct node** pp_first = &p_first;
-
-    for (int i = 1; i < 10; i++)
+    if (i == n)
     {
-        add(pp_first, i);
+        p_new->n = *pp;
+        *pp = p_new;
     }
-
-
-    //printNode(p_first);
-    //printf("%d \n", p_first);
-    printf("%d\n", p_first);
-    addInWithPointerToPointer(pp_first, 2, -1);
-    printf("%d\n", p_first);
-    printf("\n----\n");
-    //printf("%d", p_first);
-    printNode(p_first);
-
+    
 }
+
