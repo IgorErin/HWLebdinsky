17428c4fad3a4f08789398b49ff0ebb49799c82f
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-06 12:46:50 +0300

    Create 44-6.c

---
+++ 44-6.c
@@ -0,0 +1,15 @@
+int count(struct node* f,_Bool (*funBool)(struct node*))
+{
+    struct node* p = f;
+    int i = 0;
+    while (p != 0)
+    {
+        if ((*funBool)(p))
+        {
+            i++;
+        }
+        p = p->n;
+    }
+
+    return i;
+}
