2dc4f1c9f320afabeacfd17cc67e13432b6c9168
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-06 09:42:24 +0300

    Update 40-2.c

--- 40-2.c
+++ 40-2.c
@@ -1,23 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include<stdio.h>
 #include<math.h>
-#include <time.h>
+
 
 #define SIZE 500
 
 int main()
 {
-	char main_str[500] = { 0 };
-	char sub_str[500] = "abs";
+	char main_str[500] = "";
+	char sub_str[500] = "";
 
 	scanf("%s", &main_str);
 	scanf("%s", &sub_str);
-	
-	int sub_str_count = 0;	
 
-	char* tmp = main_str;	
+	int sub_str_count = 0;
 
-	
+	char* tmp = main_str;
+
+
 	while ((tmp = strstr(tmp, sub_str)) != NULL)
 	{
 		tmp++;
