ba43e49d7eb8c0bae098f81183e133cc1336b69d
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-06 12:46:05 +0300

    Create 44-5.c

---
+++ 44-5.c
@@ -0,0 +1,11 @@
+void nodePrint(struct node* f)
+{
+    struct node* p = f;
+    int i = 0;
+    printf("%d", p->d);
+    while (p != 0)
+    {
+        p = p->n;
+        printf("%d\n", p->d);
+    }
+}
