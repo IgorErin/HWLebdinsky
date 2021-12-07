d3f433931c723d256de3c14eac2681a27109ee68
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-07 20:24:57 +0300

    Create 44-11c.c

---
+++ 44-11c.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<math.h>
+#include <time.h>
+
+#define SIZE 500
+
+struct node
+{
+    int d;
+    struct node* n;
+};
+
+
+void printNode(struct node* f)
+{
+    struct node* p = f;
+
+    while (p != 0)
+    {
+        printf("%d \n", p->d);
+        p = p->n;
+    }
+
+}
+
+
+
+struct node* createRec()
+{
+    struct node* p = (struct node*)malloc(sizeof(struct node));
+    int dat;
+
+    printf("0 if this is last");
+    scanf("%d", &dat);
+    p->d = dat;
+
+    if (dat != 0)
+    {
+        p->n = createRec();
+    }
+    else {
+        p->n = 0;
+    }
+    return p;
+}
+
+
+int main()
+{
+    printNode(createRec());
+
+    
+
+}
