812fa5176cc2a98aaaf2b4a688b47ccb235ddc20
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-07 20:10:24 +0300

    Create 44-11a.c

---
+++ 44-11a.c
@@ -0,0 +1,38 @@
+void add(struct node * first_p, int dat)
+{
+    struct node* p = (struct node*)malloc(sizeof(struct node));
+    p->d = dat;
+    p->n = 0;
+    if (first_p != 0)
+    {
+        while (first_p->n != 0)
+        {
+            first_p = first_p->n;
+        }
+    }
+
+
+    first_p->n = p;
+}
+
+struct node * createWithNInSqr()
+{
+    int dat;
+    struct node* p = (struct node*)malloc(sizeof(struct node));
+    
+    printf("0 if this last \n");
+    scanf("%d", &dat);
+
+    p->n = 0;
+    p->d = dat;
+
+    while (dat != 0)
+    {
+        scanf("%d", &dat);
+        add(p, dat);
+        
+    }
+
+    return p;
+
+}
