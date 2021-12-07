c4eb31af87181ee9326d2c809a08f3a79b5c2006
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-07 20:17:18 +0300

    Create 44-11b.c

---
+++ 44-11b.c
@@ -0,0 +1,33 @@
+struct node* add(struct node* p_now, int dat)
+{
+    struct node* p_new = (struct node*)malloc(sizeof(struct node));
+
+    p_new->n = 0;
+    p_new->d = dat;
+    p_now->n = p_new;
+
+    return p_new;
+}
+
+
+struct node* createWithN()
+{
+    int dat;
+    struct node* p = (struct node*)malloc(sizeof(struct node));
+    struct node* p_now = p;
+
+    
+    printf("0 if this last\n");
+    scanf("%d", &dat);
+
+    p->d = dat;
+    p->n = 0;
+
+    while (dat != 0)
+    {
+        scanf("%d", &dat);
+        p_now = add(p_now, dat);
+    }
+
+    return p;
+}
