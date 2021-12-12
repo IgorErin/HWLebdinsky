d51b986d24d897406ec90053bf468cdcbc4163ca
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-12 22:52:04 +0300

    Update 44-25.c

--- 44-25.c
+++ 44-25.c
@@ -6,7 +6,7 @@
 struct node
 {
     int d;
-    struct node* nl, *nr;
+    struct node* nl, * nr;
 };
 
 void printNode(struct node* f)
@@ -35,61 +35,72 @@ struct node* add(int data, struct node* ex_p)
 
 struct node* create()
 {
-    struct node* p_first = (struct node*)malloc(sizeof(struct node));
-    struct node*  p_new = p_first;
+    struct node* p_new = 0;
     int data;
 
     printf("0 if last");
     scanf("%d", &data);
 
-    p_first->d = data;
-    p_first->nl = 0;
+    if (data != 0)
+    {
+        struct node* p_first = (struct node*)malloc(sizeof(struct node));
+        p_first->d = data;
+        p_first->nl = 0;
+        p_first->nr = 0;
+        p_new = p_first;
 
+        scanf("%d", &data);
+    }
     while (data != 0)
     {
-        scanf("%d", &data);
-        p_new = add(data, p_new);
         
+        p_new = add(data, p_new);
+        scanf("%d", &data);
     }
     return p_new;
 }
 
 struct node* copyDList(struct node* f)
 {
+    if (f == 0)
+    {
+        return 0;
+    }
+    
     struct node* p = f;
-    struct node* p_save = f;
+   
     struct node* p_copy = (struct node*)malloc(sizeof(struct node));
     p_copy->nl = 0;
-
-
+    
     while (p->nr != 0)
     {
-        p_copy->d = p->d;
+        
         
         if (p->nr == 0)
         {
             break;
         }
-
+        p_copy->d = p->d;
         struct node* p_new = (struct node*)malloc(sizeof(struct node));
         p_copy->nr = p_new;
         p_new->nl = p_copy;
         p_copy = p_new;
         p = p->nr;
     }
+    p_copy->d = p->d;
     p_copy->nr = 0;
-    return p_save;
+    return p_copy;
 }
 
 
 int main()
 {
-    
-    
-    printf("---------\n");
+
+
+   
     struct node* p2;
     p2 = create();
     printNode(p2);
-    p2 = copyList(p2);
+    p2 = copyDList(p2);
     printNode(p2);
 }
