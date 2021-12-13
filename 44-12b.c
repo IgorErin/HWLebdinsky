c859271f7a4f61f22208a929d39eae229f5ccf1d
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-13 10:27:21 +0300

    Update 44-12b.c

--- 44-12b.c
+++ 44-12b.c
@@ -1,27 +1,22 @@
 struct node* copyWithCases(struct node* p_first)
 {
 
-    struct node* p = (struct node*)malloc(sizeof(struct node));
+    
     if (p_first == 0)
     {
         return 0;
     }
-    else if (p_first->n == 0)
-    {
-       
-        p->n = 0;
-        p->d = p_first->d;
-        return p;
-    }
+    
     else
     {
+        struct node* p = (struct node*)malloc(sizeof(struct node));
         struct node* p_save = p;
         while (p_first->n != 0)
         {
             p->d = p_first->d;
 
-            struct node *p_new = (struct node*)malloc(sizeof(struct node));
-            
+            struct node* p_new = (struct node*)malloc(sizeof(struct node));
+
             p->n = p_new;
             p = p_new;
             p_first = p_first->n;
