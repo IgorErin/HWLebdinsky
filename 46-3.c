f90064c6810305d6713df6cfd55ee40b12e1968d
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-14 11:28:22 +0300

    Update 46-3.c

--- 46-3.c
+++ 46-3.c
@@ -37,24 +37,23 @@ void print(struct bnode* p, int n)
 
 struct bnode* fun(struct bnode* p)
 {
-    struct bnode* p_for_change, *p_save;
+    struct bnode* p_save, * p_for_move;
     if (p == 0)
     {
         return 0;
     }
-    if (p->r == 0)
+    if (p->l == 0)
     {
         return 0;
     }
-    p_save = p->r;
-    p_for_change = p->l;
+
+    p_save = p->l;
+    p_for_move = p->r;
     p->r = p_save->r;
     p->l = p_save->l;
-    p_save->r = p;
-    p_save->l = p_for_change;
-
+    p_save->l = p;
+    p_save->r = p_for_move;
     return p_save;
-
 }
 
 
@@ -63,8 +62,8 @@ int main()
 
     struct bnode* p = init(1, init(2, init(4, 0, 0), init(5, 0, 0)), init(3, init(6, 0, 0), 0));
     //print(p, 0);
-    
-    print(fun(fun(p)), 0);
+
+    print(fun(p), 0);
 
 
 }
