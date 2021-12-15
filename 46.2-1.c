4afecb43b5fe3943009388f38dd40f13f1f63511
IgorErin <47027759+IgorErin@users.noreply.github.com>
2021-12-15 10:40:43 +0300

    Create 46.2-1.c

---
+++ 46.2-1.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<math.h>
+#include <time.h>
+
+struct anode
+{
+    int dat;
+    struct anode* down, * next;
+
+};
+
+
+struct anode* g(int dat, struct anode* p_down, struct anode* p_next)
+{
+    struct anode* p = (struct anode*)malloc(sizeof(struct anode));
+    p->dat = dat;
+    p->down = p_down;
+    p->next = p_next;
+    return p;
+}
+
+void print(struct anode* p, int offset)
+{
+    if (p == 0)
+    {
+        return;
+
+    }
+
+    for (int i = 0; i < offset; i++)
+    {
+        printf(" ");
+    }
+
+    printf("%d\n", p->dat);
+    print(p->down, offset + 3);
+    print(p->next, offset);
+}
+
+int fun(struct anode* p, int n)
+{
+    if (p == 0)
+    {
+        return 0;
+    }
+    if (n == 2)
+    {
+        int count = 1;
+        while (p->next != 0)
+        {
+            count += 1;
+            p = p->next;
+        }
+        return count;
+    }
+    else
+    {
+        int sum = 0;
+        while (p != 0)
+        {
+            if (p->down != 0)
+            {
+                sum += fun(p->down, n + 1);
+            }
+            p = p->next;
+        }
+        return sum;
+    }
+    
+}
+
+int main()
+{
+
+    struct anode* p_tree = g(1, g(2, g(5, 0, g(6, 0, g(7, g(14, 0,g(15, 0,0)), 0))), g(3, g(8, 0, 0), g(4, g(9, 0, g(10, 0, 0)), 0))), 0);
+    print(p_tree, 0);
+    printf("%d", fun(p_tree, 0));
+
+}
